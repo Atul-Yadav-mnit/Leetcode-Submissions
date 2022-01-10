e5e90de65aa98829bcfa302f8d16ab5ab3df60ef
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-01-10 16:58:27 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static int fun(Node head)
+    {
+        if(head == null)
+        {
+            return 1;
+        }
+        int x = head.data+fun(head.next);
+        head.data = x%10;
+        return x/10;
+    }
+    public static Node addOne(Node head) 
+    { 
+        //code here.
+        int x = fun(head);
+        if(x != 0)
+        {
+            Node temp = head;
+            head = new Node(x);
+            head.next = temp;
+        }
+        return head;
+    }
+}
