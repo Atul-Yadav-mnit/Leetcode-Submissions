21e782111cf98574357630f4620df660363e88e8
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-01-15 01:28:21 +0530

    Time: 468 ms (6.38%), Space: 51.8 MB (36.17%) - LeetHub

--- 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.java
+++ 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.java
@@ -1,85 +1,46 @@
 class Solution {
-    public int minCharacters(String s1, String s2) {
-        if(s1.length() == 1 && s2.length()==1)
+    public int fun(String a, String b)
+    {
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<25;i++)
         {
-            return 0;
+            char c = (char)(i+(int)'a');
+            int c1 = 0;
+            for(int j=0;j<a.length();j++)
+            {
+                if(a.charAt(j) > c)
+                {
+                    c1++;
+                }
+            }
+            for(int j=0;j<b.length();j++)
+            {
+                if(b.charAt(j) <= c)
+                {
+                    c1++;
+                }
+            }
+            ans = Math.min(ans,c1);
         }
-        char a[] = s1.toCharArray();
-        char b[] = s2.toCharArray();
-        int fra[] = new int[26];
-        int frb[] = new int[26];
-        for(int i=0;i<a.length;i++)
+        return ans;
+    }
+    public int minCharacters(String a, String b) {
+        int ans = Integer.MAX_VALUE;
+        int c[] = new int[26];
+        for(int j=0;j<a.length();j++)
         {
-            fra[a[i]-'a']++; 
+            c[a.charAt(j)-'a']++;
         }
-        for(int i=0;i<b.length;i++)
+        for(int j=0;j<b.length();j++)
         {
-            frb[b[i]-'a']++; 
+            c[b.charAt(j)-'a']++;
         }
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int min = Math.min(a.length+frb[0],b.length+fra[0]);
         for(int i=0;i<26;i++)
         {
-            min = Math.min(a.length+b.length-fra[i]-frb[i],min);
+            ans = Math.min(ans,a.length()+b.length()-c[i]);
         }
-        // keeping a smaller
-       
-        for(int i=0;i<a.length;i++)
-        {
-            // delete 0 from a and fing from b
-            char c = a[a.length-1-i];
-            if(c == 'z')
-            {
-                continue;
-            }
-            int d = i;
-            int l = 0;
-            int h = b.length;
-            while(l<h)
-            {
-                int m = l+(h-l)/2;
-                if(b[m] > c)
-                {
-                    h = m;
-                }
-                else
-                {
-                    l = m+1;
-                }
-            }
-            //if(l< a.length && a[l]<) 
-            d += (l);
-          //System.out.println(d);
-            min = Math.min(d,min);
-        }
-        
-        for(int i=0;i<b.length;i++)
-        {
-            // delete 0 from a and fing from b
-            char c = b[b.length-1-i];
-            if(c == 'z')
-            {
-                continue;
-            }
-            int d = i;
-            int l = 0;
-            int h = a.length;
-            while(l<h)
-            {
-                int m = l+(h-l)/2;
-                if(a[m] > c)
-                {
-                    h = m;
-                }
-                else
-                {
-                    l = m+1;
-                }
-            }
-            d += (l);
-            min = Math.min(d,min);
-        }
-        return min;
+        ans = Math.min(fun(a,b),ans);
+        ans = Math.min(fun(b,a),ans);
+        return ans;
     }
 }
