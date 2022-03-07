ce918c28423ae846e36bd8feeb3f1ff4fbdc418e
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-03-07 13:20:12 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.java
@@ -0,0 +1,124 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int K)
+    {
+        //Add your code here.
+        PriorityQueue<Node> p = new PriorityQueue<Node>((n1,n2) -> n1.data-n2.data);
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=null)
+            p.add(arr[i]);
+        }
+        Node ans = null;
+        Node temp = null;
+        while(p.size()>0)
+        {
+            Node t = p.poll();
+            if(ans == null)
+            {
+                ans = t;
+                temp = ans;
+                Node tt = t.next;
+                if(tt!=null)
+                {
+                    p.add(tt);
+                }
+                temp.next = null;
+            }
+            else
+            {
+                temp.next = t;
+                Node tt = t.next;
+                if(tt!=null)
+                {
+                    p.add(tt);
+                }
+                temp = temp.next;
+                temp.next = null;
+            }
+        }
+        return ans;
+    }
+}
