d2c542ea7c6daa6f4a3c60067d677fe0b3881edf
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-03-16 08:11:05 +0530

    Time: 5 ms (43.81%), Space: 45 MB (52.55%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,47 +1,26 @@
 class Solution {
-    public boolean t[][],b[][];
-    public boolean fun(int push[], int pop[], int x, int y,Stack<Integer> s)
-    {
-        if(b[x][y])
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> s = new Stack<Integer>();
+        int x = 0;
+        int y = 0;
+        while(x<pushed.length)
         {
-            return t[x][y];
-        }
-        if(x==push.length)
-        {
-            while(y<pop.length)
+            if(s.isEmpty()==false && s.peek()==popped[y])
             {
-                if(s.pop()!=pop[y])
-                {
-                    b[x][y] = true;
-                    return t[x][y] = false;
-                }
+                s.pop();
                 y++;
             }
-            b[x][y] = true;
-            return t[x][y] = true;
+            else
+            {
+                s.push(pushed[x]);
+                x++;
+            }
         }
-        if(s.isEmpty()==false && s.peek() == pop[y])
+        while(s.isEmpty()==false && s.peek()==popped[y])
         {
-            int z = s.pop();
-            boolean ans1 = fun(push,pop,x,y+1,s);
-            s.push(z);
-            s.push(push[x]);
-            boolean ans2 = fun(push,pop,x+1,y,s);
-            b[x][y] = true;
-            return t[x][y] = ans1 || ans2;
+            s.pop();
+            y++;
         }
-        else
-        {
-            s.push(push[x]);
-            boolean ans = fun(push,pop,x+1,y,s);
-            b[x][y] = true;
-            return t[x][y] =  ans;
-        }
-    }
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
-        b = new boolean[pushed.length+1][pushed.length+1];
-        t = new boolean[pushed.length+1][pushed.length+1];
-        Stack<Integer> s = new Stack<Integer>();
-        return fun(pushed,popped,0,0,s);
+        return s.isEmpty() && y==popped.length;
     }
 }
