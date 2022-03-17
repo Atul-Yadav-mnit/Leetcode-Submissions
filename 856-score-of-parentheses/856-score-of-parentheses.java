3338cf4632aabf5d700827c4f85a63ad906be0bf
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-03-17 07:22:31 +0530

    Time: 1 ms (68.54%), Space: 42.3 MB (18.14%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,44 +1,32 @@
 class Solution {
-    public int fun(String s, int l , int h)
-    {
-      //  System.out.println(l+" "+h);
-        
-        
-        Stack<Integer> st = new Stack<Integer>();
-        int ans =0;
-        int cur = l;
-        for(int i=l;i<=h;i++)
+    public Stack<Integer> sum = new Stack<Integer>();
+    public Stack<Character> s = new Stack<Character>();
+    public int ans;
+    public int scoreOfParentheses(String st) {
+        ans = 0;
+        for(int i=0;i<st.length();i++)
         {
-            char c = s.charAt(i);
-            if(c == '(')
+            char x = st.charAt(i);
+            if(x == '(')
             {
-                st.push(i);
+                s.push(x);
+                sum.push(0);
             }
             else
             {
-                int j = st.pop();
-            }
-            
-            if(st.isEmpty())
-            {
-               // System.out.println((cur)+" "+(i));
-               
-                if(i-1==cur)
+                s.pop();
+                int y = Math.max(1,sum.pop());
+                if(s.isEmpty())
                 {
-                    ans+=1;
+                    ans += y;
                 }
                 else
                 {
-                    ans+= 2*fun(s,cur+1,i-1); 
+                    int z = sum.pop();
+                    sum.push(z+2*y);
                 }
-                
-                cur = i+1;
             }
-           
         }
         return ans;
-    }
-    public int scoreOfParentheses(String s) {
-       return fun(s,0,s.length()-1);
     }
 }
