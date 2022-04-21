10e236934e8fd8204b834f66d348625d1b4fd210
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-04-21 17:15:47 +0530

    Time: 670 ms (5.03%), Space: 44.9 MB (98.89%) - LeetHub

---
+++ 825-friends-of-appropriate-ages/825-friends-of-appropriate-ages.java
@@ -0,0 +1,60 @@
+class Solution {
+    //  x+14 >= 2*y
+    public int numFriendRequests(int[] age) {
+        int c = 0;
+        Arrays.sort(age);
+        for(int i=0;i<age.length;i++)
+        {
+            int hig = age[i];
+            int low = (age[i]+14)/2+1;
+            if(age[i] < 100)
+            {
+                hig = Math.min(100,hig);
+            }
+           // System.out.println(low+" "+hig);
+            int li = age.length, hi = -1;
+            int l = 0;
+            int h = age.length-1;
+            while(l<=h)
+            {
+                int m = l+(h-l)/2;
+                
+                if(age[m] >= low)
+                {
+                  
+                    li = m;
+                    h = m-1;
+                }
+                else
+                {
+                    l = m+1;
+                }
+            }
+            l = 0;
+            h = age.length-1;
+            while(l<=h)
+            {
+                int m = l+(h-l)/2;
+                if(age[m] <= hig)
+                {
+                    hi = m;
+                    l = m+1;
+                }
+                else
+                {
+                    h = m-1;
+                }
+            }
+            
+            int x = 0;
+            if(i>=li && i<=hi)
+            {
+                x--;
+            }
+            x+=Math.max(0,hi-li+1);
+           System.out.println(li+" "+hi+" "+x);
+            c+=x;
+        }
+        return c;
+    }
+}
