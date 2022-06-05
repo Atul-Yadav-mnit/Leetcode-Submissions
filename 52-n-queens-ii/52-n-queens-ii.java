4c5e2a75eb42b1b9eafd2087432c82db845d6aa5
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-06-05 08:10:17 +0530

    Time: 39 ms (5.87%), Space: 41.2 MB (43.17%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,113 @@
+class Solution {
+    List<List<String>> q;
+    List<List<String>> ans = new ArrayList<List<String>>();
+    public boolean isvalid(int x, int y)
+    {
+        int n = q.size();
+       // System.out.println(x+" "+y+" "+n);
+        for(int i=0;i<n;i++)
+        {
+            if(i!=y && q.get(x).get(i).equals("Q"))
+            {
+                return false;
+            }
+            if(i!=x && q.get(i).get(y).equals("Q"))
+            {
+                return false;
+            }
+        }
+        int a = x-1;
+        int b = y-1;
+        while(a>=0 && b>=0)
+        {
+            if(q.get(a).get(b).equals("Q"))
+            {
+                return false;
+            }
+            a--;
+            b--;
+        }
+        
+        a = x-1;
+        b = y+1;
+        while(a>=0 && b<n)
+        {
+            if(q.get(a).get(b).equals("Q"))
+            {
+                return false;
+            }
+            a--;
+            b++;
+        }
+        
+        a = x+1;
+        b = y+1;
+        while(a<n && b<n)
+        {
+            if(q.get(a).get(b).equals("Q"))
+            {
+                return false;
+            }
+            a++;
+            b++;
+        }
+        
+        a = x+1;
+        b = y-1;
+        while(a<n && b>=0)
+        {
+            if(q.get(a).get(b).equals("Q"))
+            {
+                return false;
+            }
+            a++;
+            b--;
+        }
+        
+        return true;
+    }
+    
+    public void fun(int x)
+    {
+        int n = q.size();
+        if(x == n)
+        {
+            List<String> t = new ArrayList<String>();
+            for(int i=0;i<n;i++)
+            {
+                StringBuilder tt = new StringBuilder();
+                for(int j=0;j<n;j++)
+                {
+                    tt.append(q.get(i).get(j));
+                }
+                t.add(tt.toString());
+            }
+            ans.add(t);
+            return;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(isvalid(x,i))
+            {
+                q.get(x).set(i,"Q");
+                fun(x+1);
+                q.get(x).set(i,".");
+            }
+        }
+        return ;
+    }
+    
+    public int totalNQueens(int n) {
+        q = new ArrayList<List<String>>();
+        for(int i=0;i<n;i++)
+        {
+            q.add(new ArrayList<String>());
+            for(int j=0;j<n;j++)
+            {
+                q.get(i).add(".");
+            }
+        }
+        fun(0);
+        return ans.size();
+    }
+}
