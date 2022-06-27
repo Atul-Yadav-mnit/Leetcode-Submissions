f1912dcb6ea27c88e2108592ad164c5f26510b7e
Atul-Yadav-mnit <68809688+Atul-Yadav-mnit@users.noreply.github.com>
2022-06-27 07:22:06 +0530

    Added solution - LeetHub

---
+++ Change Bits - GFG/change-bits.java
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            int[] ans = ob.changeBits(N);
+            System.out.println(ans[0]+ " "+ ans[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int[] changeBits(int N) {
+        // code here
+        int x = (int)(Math.log(N)/Math.log(2));
+        int ans = (int)Math.pow(2,x+1);
+        int a[] = new int[2];
+        a[0] = ans-N-1;
+        a[1] = ans-1;
+        return a;
+    }
+};
